3ac8668c04216c0f9510d332e3fe952d2bea96b5
sofiabytes <dev@sofiabytes.net>
2026-08-23 07:50:27 +0100

    Correct manifest

--- main.ts
+++ main.ts
@@ -30,8 +30,6 @@ export default class PixNotePlugin extends Plugin {
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new PixNoteSettingTab(this.app, this));
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
